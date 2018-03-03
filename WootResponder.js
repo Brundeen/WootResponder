e5d6ad159a0076bc0f0c8de9c9e6ec46d24ba91b
Brundeen <anonymousbrandon@yahoo.com>
2018-03-03 00:02:39 -0500

    Update WootResponder.js

--- WootResponder.js
+++ WootResponder.js
@@ -1,22 +1,22 @@
 var bot = {		
-      				   				     					
-    eventChat: function(obj) {							
-	var msg = obj.message,									
-		msgArray = msg.split(" ");
-			for (i = 0; i < msgArray.length; i++) {
-				switch (msgArray[i]) {
-										
-					case 'bop':
-					case 'woot':
-					case 'dance':					
-					case '!bop':
-						$('#woot').click();
+  eventChat: function(obj) {							
+    var msg = obj.message,									
+      msgArray = msg.split(" ");
+        for (i = 0; i < msgArray.length; i++) {
+          switch (msgArray[i]) {
+            case 'bop':
+	    case 'BOP':
+	    case 'woot':
+            case 'WOOT':
+	    case 'dance':
+	    case 'DANCE':					
+	    case '!bop':
+	      $('#woot').click();
 									
-							      }
-					}
-									
-			} 
-    }
+              }
+           }									
+        } 
+     }
 										
 API
 .on(API.CHAT, $.proxy(bot.eventChat, this));
